f2518bbe8a663738b789ffe9237aeae96bb28e7b
Tariqnawaz <31206475+Tariqnawaz@users.noreply.github.com>
2020-12-12 19:14:49 +0400

    removed spaces & console log

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import "./styles.css";
-import {emp} from "./employeeData"
+import { emp } from "./employeeData";
 
 //console.log(JSON.stringify(emp, null, 2));
-console.log(Array.isArray(emp))
+console.log(Array.isArray(emp));
 //let emp1 = emp.slice(0,1);
 //console.log(JSON.stringify(emp1, null , 2))
 
-console.log("Array result from server == ",emp.length);
+console.log("Array result from server == ", emp.length);
 let emp3 = emp
-          .filter(e=> e.gender === "Male")
-          //.map(e => e.first_name.toUpperCase()) // it will return single field
-          .map(e => {
-            e.first_name.toUpperCase()
-            return e;
-          })
-          .sort((a,b) => (a.first_name > b.first_name)?1: (a.first_name<b.first_name)?-1:0)
-          
+  .filter((e) => e.gender === "Male")
+  //.map(e => e.first_name.toUpperCase()) // it will return single field
+  .map((e) => {
+    e.first_name.toUpperCase();
+    return e;
+  })
+  .sort((a, b) =>
+    a.first_name > b.first_name ? 1 : a.first_name < b.first_name ? -1 : 0
+  );
 
-
-
-function compare( a, b ) {
-  if ( a.first_name < b.first_name ){
+function compare(a, b) {
+  if (a.first_name < b.first_name) {
     return -1;
   }
-  if ( a.first_name > b.first_name ){
+  if (a.first_name > b.first_name) {
     return 1;
   }
   return 0;
@@ -34,50 +33,49 @@ function compare( a, b ) {
 // // });
 
 // emp3.sort( compare );
+console.log("Filtered Array result == ", emp3.length);
+const firstEmp = emp3.shift(); // removed 1st element
+console.log("Filtered Array result shift() == ", emp3.length);
 //console.log(JSON.stringify(emp3, null , 2))
-console.log("Filtered Array result == ",emp3.length);
-const firstEmp = emp3.shift() // removed 1st element
-console.log("Filtered Array result shift() == ",emp3.length);
+emp3.unshift(firstEmp); // added element on zero index
+console.log("Filtered Array result unshift() == ", emp3.length);
+const empFirst4 = emp.slice(0, 4); // slice first4 - original list not effected
+console.log("Slice() Array result == ", empFirst4.length);
+console.log("Filtered Array result after slice() == ", emp3.length);
+emp3.concat(empFirst4);
 //console.log(JSON.stringify(emp3, null , 2))
-emp3.unshift(firstEmp)    // added element on zero index
-console.log("Filtered Array result unshift() == ",emp3.length);
-const empFirst4 = emp.slice(0,4) // slice first4 - original list not effected
-console.log("Slice() Array result == ",empFirst4.length);
-console.log("Filtered Array result after slice() == ",emp3.length);
-emp3.concat(empFirst4) 
-//console.log(JSON.stringify(emp3, null , 2))
-console.log("Concat() Array result == ",emp3.length);
-emp3.splice(1, 0, 'Feb');
-console.log("splice() Array result == ",emp3.length);
+console.log("Concat() Array result == ", emp3.length);
+emp3.splice(1, 0, "Feb");
+console.log("splice() Array result == ", emp3.length);
 
-for(const [index,element] of emp3.entries()){
+for (const [index, element] of emp3.entries()) {
   //console.log("Entries() index : ", index, " object : ", JSON.stringify(element, null, 2))
 }
 
-const findEmpIndex = emp3.findIndex(e=> e.id === 9)
+const findEmpIndex = emp3.findIndex((e) => e.id === 9);
 //const arrayIncludes = emp3.includes(e => e.id === 9)
 //console.log(arrayIncludes)
-let findEmp = {}
-if(findEmpIndex !== -1){
-  findEmp = emp3.find(e=> e.id === 9)
-  console.log("Array find() ==>",JSON.stringify(findEmp))
+let findEmp = {};
+if (findEmpIndex !== -1) {
+  findEmp = emp3.find((e) => e.id === 9);
+  console.log("Array find() ==>", JSON.stringify(findEmp));
 }
 
-for(const key of emp3){
+for (const key of emp3) {
   console.log("array keys : ", key);
 }
 console.log("array join : ", emp3.join("_"));
 console.log("array join : ", emp3.reverse());
-for(const value of emp3){
+for (const value of emp3) {
   console.log("array values() : ", value);
 }
 
 /***  Objects  */
-const empObj = emp3.shift()
+const empObj = emp3.shift();
 console.log("Obect to manuplate : ", empObj);
-const obj = Object.assign({}, empObj)
+const obj = Object.assign({}, empObj);
 console.log("Obect assign() : ", obj);
-for(const [key, value] of Object.entries(obj)){
+for (const [key, value] of Object.entries(obj)) {
   console.log("Obect entries() : key", key, " value : ", value);
 }
 console.log("Object keys() ", Object.keys(obj));
